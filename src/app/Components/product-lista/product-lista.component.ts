bb44585151da1191724172f21e3dcd7905607612
Gabbs1110 <gabb8091@gmail.com>
2019-05-13 21:42:45 -0300

    protótipo

---
+++ src/app/Components/product-lista/product-lista.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-lista',
+  templateUrl: './product-lista.component.html'
+})
+export class ProductListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//Root
 import { AppComponent } from './app.component';
+
+
+//shared
+import { HeadbarComponent } from './Components/Shared/headbar/headbar.component';
+import { SubMenuComponent } from './Components/Shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+
+//components
+import { ProductListaComponent } from './Components/product-lista/product-lista.component';
+
+//pages
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { SignupPageComponent } from './Pages/signup-page/signup-page.component';
+import { CartPageComponent } from './Pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    ProductListaComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
